7b4f1207b1a512f93e8c723f170b2afae8274cb7
kowaliks <kowal96@gmail.com>
2020-05-28 19:10:40 +0200

    complexity measure added

--- diff_evolution/cec_metrics.py
+++ diff_evolution/cec_metrics.py
@@ -10,11 +10,13 @@ import numpy as np
 from statistics import median, mean, stdev
 import os
 import json
+import math
 
 MAX_FES_FACTOR = 10000
 TARGET_VALUE_FACTOR = 100
 BOUNDS_1D = [(-100, 100)]
 PROCESSES_NUM = 4
+COMPLEXITY_MEASURE_FUNCTION_NUM = 1 # TMP TODO: Change to 18
 
 
 def run_single_problem(de: DifferentialEvolution, dims: int, func_num: int, max_fes = None):
@@ -65,9 +67,71 @@ def generate_output(algo_results, dims, func_num, output_path):
     with open(os.path.join(output_path, f'metrics_{func_num}_{dims}.json'), 'w') as f:
         json.dump(metrics, f)
 
-if __name__ == '__main__':
+
+def t0_function():
+    for _ in range(1000000):
+        x = 0.55 # changed!!! TODO: write about it in the report
+        x = x + x
+        x = x / 2
+        x = x * x
+        x = math.sqrt(x)
+        x = math.log(x)
+        x = math.exp(x)
+        x = x / (x+2)
+
+def t1_function(dims):
+    v = np.random.rand(dims)
+
+    min_b, max_b = BOUNDS_1D[0][0], BOUNDS_1D[0][1]
+    diff = np.fabs(min_b - max_b)
+    v_denorm = min_b + v * diff
+
+    for _ in range(200000):
+        cec17_test_func(v_denorm, dims, func_num=COMPLEXITY_MEASURE_FUNCTION_NUM)
+
+def t2_function(algorithm, dims):
+    run_single_problem(algorithm, dims, COMPLEXITY_MEASURE_FUNCTION_NUM, 200000)
+
+
+def measure_performance(output_path):
     for dims in [10, 30, 50]:
         for func_num in range(1, 30):
+            if func_num == 2:
+                continue
             print(f'Running test for function {func_num}, {dims} dims.')
             res = run_multi_problems([(dims, func_num)] * 51)
-            generate_output(res, 10, 1, '.')
+            generate_output(res, dims, func_num, output_path)
+
+
+def measure_complexity(algorithm, output_path):
+    dims = [10, 30, 50]
+    res_table = np.zeros((len(dims), 4))
+    for i, dims in enumerate(dims):
+        start = time.time()
+        t0_function()
+        end = time.time()
+        t0 = end - start
+
+        start = time.time()
+        t1_function(dims)
+        end = time.time()
+        t1 = end - start
+
+        t2s = []
+        for _ in range(5):
+            start = time.time()
+            t2_function(algorithm, dims)
+            end = time.time()
+            t2s.append(end - start)
+
+        t2prim = mean(t2s)
+
+        res_table[i, :] = [t0, t1, t2prim, (t2prim - t1)/t0]
+
+        np.savetxt(os.path.join(output_path, 'complexity.txt'), res_table, delimiter=',')
+
+if __name__ == '__main__':
+    de = ConstantDE()
+
+    # measure_performance()
+    measure_complexity(de, '.')

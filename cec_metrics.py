8aef176b1a97151c3f09435de0b372876b6dc810
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2020-05-29 17:55:04 +0200

    Move cec metrics out of package

--- cec_metrics.py
+++ cec_metrics.py
@@ -1,26 +1,26 @@
-from diff_evolution.algo import ConstantDE, DifferentialEvolution
-from diff_evolution.cec17_functions import cec17_test_func
-from diff_evolution.algo_control import AlgorithmControl, RECORDING_POINTS
-
-
-import time
 import itertools
-from functools import partial
-from multiprocessing import Pool
-import numpy as np
-from statistics import median, mean, stdev
-import os
 import json
 import math
+import os
+import time
+from functools import partial
+from multiprocessing import Pool
+from statistics import mean, median, stdev
+
+import numpy as np
+
+from diff_evolution.algo import ConstantDE, DifferentialEvolution
+from diff_evolution.algo_control import RECORDING_POINTS, AlgorithmControl
+from diff_evolution.cec17_functions import cec17_test_func
 
 MAX_FES_FACTOR = 10000
 TARGET_VALUE_FACTOR = 100
 BOUNDS_1D = [(-100, 100)]
 PROCESSES_NUM = 4
-COMPLEXITY_MEASURE_FUNCTION_NUM = 1 # TMP TODO: Change to 18
+COMPLEXITY_MEASURE_FUNCTION_NUM = 18
 
 
-def run_single_problem(problem, de: DifferentialEvolution, max_fes = None):
+def run_single_problem(problem, de: DifferentialEvolution, max_fes=None):
     dims, func_num = problem
     if max_fes is None:
         max_fes = MAX_FES_FACTOR * dims
@@ -39,42 +39,47 @@ def run_single_problem(problem, de: DifferentialEvolution, max_fes = None):
 
     return algo_control.recorded_values, algo_control.error()
 
+
 def run_multi_problems(algorithm, problems):
     run_process = partial(run_single_problem, de=algorithm)
     with Pool(PROCESSES_NUM) as p:
         resulsts = p.map(run_process, problems)
         return resulsts
 
+
 def generate_output(algo_results, dims, func_num, output_path):
     res_table = np.zeros((len(RECORDING_POINTS), len(algo_results)))
     for i, algo_result in enumerate(algo_results):
-        res_table[:,  i] = algo_result[0]
+        res_table[:, i] = algo_result[0]
 
     errors = [el[1] for el in algo_results]
     metrics = {
-        'best': min(errors),
-        'worst': max(errors),
-        'median': median(errors),
-        'mean': mean(errors),
-        'std': stdev(errors)
+        "best": min(errors),
+        "worst": max(errors),
+        "median": median(errors),
+        "mean": mean(errors),
+        "std": stdev(errors),
     }
 
-    np.savetxt(os.path.join(output_path, f'{func_num}_{dims}.txt'), res_table, delimiter=',')
+    np.savetxt(
+        os.path.join(output_path, f"{func_num}_{dims}.txt"), res_table, delimiter=","
+    )
 
-    with open(os.path.join(output_path, f'metrics_{func_num}_{dims}.json'), 'w') as f:
+    with open(os.path.join(output_path, f"metrics_{func_num}_{dims}.json"), "w") as f:
         json.dump(metrics, f)
 
 
 def t0_function():
     for _ in range(1000000):
-        x = 0.55 # changed!!! TODO: write about it in the report
+        x = 0.55  # changed!!! TODO: write about it in the report
         x = x + x
         x = x / 2
         x = x * x
         x = math.sqrt(x)
         x = math.log(x)
         x = math.exp(x)
-        x = x / (x+2)
+        x = x / (x + 2)
+
 
 def t1_function(dims):
     v = np.random.rand(dims)
@@ -86,16 +91,17 @@ def t1_function(dims):
     for _ in range(200000):
         cec17_test_func(v_denorm, dims, func_num=COMPLEXITY_MEASURE_FUNCTION_NUM)
 
+
 def t2_function(algorithm, dims):
     run_single_problem((dims, COMPLEXITY_MEASURE_FUNCTION_NUM), algorithm, 200000)
 
 
 def measure_performance(algorithm, output_path):
     for dims in [10, 30, 50]:
-        for func_num in range(1, 30):
+        for func_num in range(1, 31):
             if func_num == 2:
                 continue
-            print(f'Running test for function {func_num}, {dims} dims.')
+            print(f"Running test for function {func_num}, {dims} dims.")
             res = run_multi_problems(algorithm, [(dims, func_num)] * 51)
             generate_output(res, dims, func_num, output_path)
 
@@ -123,12 +129,15 @@ def measure_complexity(algorithm, output_path):
 
         t2prim = mean(t2s)
 
-        res_table[i, :] = [t0, t1, t2prim, (t2prim - t1)/t0]
+        res_table[i, :] = [t0, t1, t2prim, (t2prim - t1) / t0]
 
-        np.savetxt(os.path.join(output_path, 'complexity.txt'), res_table, delimiter=',')
+        np.savetxt(
+            os.path.join(output_path, "complexity.txt"), res_table, delimiter=","
+        )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     de = ConstantDE()
 
-    measure_performance(de, '.')
-    measure_complexity(de, '.')
+    measure_performance(de, ".")
+    measure_complexity(de, ".")

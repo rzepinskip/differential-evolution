0b70db5d36619c0b8b2a2dda8993208c21d0c190
kowaliks <kowal96@gmail.com>
2020-06-19 14:20:25 +0200

    welch test improved and fixed

--- welch_test.py
+++ welch_test.py
@@ -1,4 +1,5 @@
 import os
+import csv
 
 from scipy import stats
 import numpy as np
@@ -21,10 +22,15 @@ def compare_algorithms_sinlge_problem(alg1_results_path, alg2_results_path, conf
 
     pvalue = stats.ttest_ind(alg1_results, alg2_results, equal_var=False)[1]
 
-    if pvalue <= (1-confidence)/2:
-        return True if alg1_mean < alg2_mean else False
+    result = None
 
-    return None
+    if pvalue / 2 <= (1-confidence):
+        if alg1_mean < alg2_mean:
+            result = True
+        else:
+            result = False
+
+    return alg1_mean, alg2_mean, result
 
 
 @click.command()
@@ -41,32 +47,60 @@ def compare_algorithms_sinlge_problem(alg1_results_path, alg2_results_path, conf
     type=click.Path(exists=True, file_okay=False, dir_okay=True),
 )
 @click.option(
+    "--output",
+    required=True,
+    help="Output file folder",
+    type=click.Path(exists=True, file_okay=False, dir_okay=True),
+)
+@click.option(
     "--confidence", "-c", default=0.95, show_default=True, help="Welch's t-test confidence", type=float
 )
-def compare_algorithms(alg1_dir, alg2_dir, confidence = 0.95):
+def compare_algorithms(alg1_dir, alg2_dir, output, confidence = 0.95):
     counter = {
         True: 0,
         False: 0,
         None: 0
     }
-    alg1_name = None
-    alg2_name = None
+
+    alg1_name = os.path.basename(alg1_dir)
+    alg2_name = os.path.basename(alg2_dir)
+
+    out_file_path = os.path.join(output, f"{alg1_name}_vs_{alg2_name}.csv")
+
+    alg1_better_name = f"{alg1_name} better than {alg2_name}"
+    fieldnames = ['Func.', alg1_name, alg2_name, alg1_better_name]
+
+    results = []
+
     for f1 in os.listdir(alg1_dir):
         if f1.endswith(".txt"):
-            if not alg1_name:
-                alg1_name = f1[:f1.find('_')]
             file_subname = f1[f1.find('_'):]
             for f2 in os.listdir(alg2_dir):
                 if f2.endswith(file_subname):
-                    if not alg2_name:
-                        alg2_name = f2[:f2.find('_')]
-                    result = compare_algorithms_sinlge_problem(
+                    alg1_result, alg2_result, alg1_better = compare_algorithms_sinlge_problem(
                         os.path.join(alg1_dir, f1),
                         os.path.join(alg2_dir, f2),
                         confidence
                     )
-                    counter[result] += 1
+                    counter[alg1_better] += 1
+                    function_num = int(file_subname[1:file_subname.rfind('_')])
 
+                    res = {
+                        'Func.': function_num,
+                        alg1_name: f"{alg1_result:.2E}",
+                        alg2_name: f"{alg2_result:.2E}",
+                        alg1_better_name: alg1_better
+                    }
+                    results.append(res)
+
+    results.sort(key = lambda x: x['Func.'])
+
+    with open(out_file_path, mode="w", newline="\n") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for res in results:
+            writer.writerow(res)
 
     counter[f'{alg1_name} better'] = counter.pop(True)
     counter[f'{alg2_name} better'] = counter.pop(False)
